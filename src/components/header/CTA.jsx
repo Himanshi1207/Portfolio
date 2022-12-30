bb2365229a484202d30866ddb31d372049035259
Himanshi <kanojiahimanshi1221@gmail.com>
2022-12-31 00:02:48 +0530

    added dark and light mode

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CV from '../../assets/Himanshi_resume.pdf'
 const CTA = () => { 
+  const [theme, setTheme]=useState("light_theme");
+  const toggleTheme=()=>{
+    // alert("hi")
+    theme==="light_theme"?setTheme("dark_theme"):setTheme("light_theme")
+  }
+  useEffect(()=>{
+    document.body.className=theme;
+  },[theme]);
   return (
     <div className="cta">
         <a href={CV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary '>Let's Talk</a>
+        <a href="#" className='btn' onClick={()=>toggleTheme()}>{theme==='light_theme'?'Dark Mode':'Light Mode'}</a>
+        {/* <a href="https://www.behance.net/himanshikanojia" className='toggle' target="_blank"><AiFillBehanceCircle/></a> */}
     </div>
   )
 }
